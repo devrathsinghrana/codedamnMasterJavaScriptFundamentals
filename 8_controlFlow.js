d158787a0fdb5455c59fdc53e2e1047647696040
devrathrana <devrath.rana@bold.com>
2024-12-30 11:43:18 +0530

    js

---
+++ 8_controlFlow.js
@@ -0,0 +1,51 @@
+const arr = [1, 2, 3];
+arr[6] = 23;
+
+for (let val of arr) {
+  console.log("val>>>", val);
+}
+
+for (let key in arr) {
+  console.log("key>>>", key);
+}
+
+for (let i = 0; i < 3; i++) {
+  console.log("i>>>", i);
+}
+
+let count = 0;
+while (count < 3) {
+  console.log("count>>>", count);
+  count++;
+}
+
+let counting = 5;
+do {
+  console.log("counting>>>", counting);
+  counting++;
+} while (counting < 5);
+
+const color = "red";
+if (color == "red") {
+  console.log("first red");
+}
+
+if (color == "red") {
+  console.log("second red");
+}
+
+const myName = "Bhalla";
+
+if (myName == "Bhalla") {
+  console.log("first Bhalla");
+} else if (myName == "Bhalla") {
+  console.log("second Bhalla");
+}
+
+if (myName == "Kalla") {
+  console.log("first red");
+} else {
+  console.log("Else consoled");
+}
+
+// Logical operators for strong password check logic
