c4098e04650f8e3f48de7b5ddc0034fe07d94246
devrathrana <devrath.rana@bold.com>
2024-12-30 23:25:34 +0530

    js

--- 8_controlFlow.js
+++ 8_controlFlow.js
@@ -49,3 +49,34 @@ if (myName == "Kalla") {
 }
 
 // Logical operators for strong password check logic
+const password = "abc@gmail.pass";
+
+if (password.length < 8 && password.includes("@")) {
+  console.log("MODERATE PASSWORD");
+} else if (password.length > 12) {
+  console.log("STRONG PASSWORD");
+} else {
+  console.log("WEAK PASSWORD");
+}
+
+// break continue to make rating logic
+const ratings = [1, 2, 3, 4, 5, 6];
+for (let key in ratings) {
+  const rating = ratings[key];
+  if (rating === 1) {
+    console.log("SKIP");
+    continue;
+  }
+
+  if (rating === 6) {
+    console.log("BREAK");
+    break;
+  }
+  console.log(`Watched movie rated - ${rating}`);
+}
+console.log(`out of rating system`)
+// switch to make rating logic
+
+// global local scope using nested if
+
+// ternary operator
