7dd0e4fb2257e11567dbe05468137e9030991671
devrathrana <devrath.rana@bold.com>
2024-12-31 00:52:54 +0530

    JS

---
+++ 9_functions.js
@@ -0,0 +1,20 @@
+//defining or declaring function and invoking it
+
+//regular function 
+
+//variable function
+
+//arguments(value being passed) and parameters(value being captured)
+
+//default values of parameters
+
+//default value of return key is undefined
+
+//arrow functions
+
+const capitaliseWord = word => `${word.charAt(0).toUpperCase()}${word.substring(1)}`;
+
+console.log(capitaliseWord("hello")); // Output: 'Hello'
+console.log(capitaliseWord("mom")); // Output: 'Mom'
+console.log(capitaliseWord("dAD")); // Output: 'DAD'
+
