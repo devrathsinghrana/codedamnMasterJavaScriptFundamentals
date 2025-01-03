f054a054a1b2181b9fb269ba74fa20ba9d1d945a
devrathrana <devrath.rana@bold.com>
2025-01-03 22:12:16 +0530

    js

--- 11_DOM.js
+++ 11_DOM.js
@@ -21,10 +21,11 @@ console.log("pTagElem>>>", pTagElem);
 console.log("listItemsByClass>>>", listItemsByClass);
 console.log("heading>>>", heading);
 
+//.style for adding styles instead of overriding done by setAttribute. js cant have hyphenated property names so use camelcase property names as hypen will be considered as minus by js for a property name
 //Modifying DOM innerText innerHTML. adding html li in whatever position inside ul using innerhtml
 
+const listContainer = document.querySelector(".list");
 const addLiInExistingList = (position, listItemToAdd) => {
-  const listContainer = document.querySelector(".list");
   const listItems = document.querySelectorAll(".list-item");
   const existingListItemsCount = listItems.length;
   if (position > existingListItemsCount || position < 1)
@@ -40,6 +41,24 @@ addLiInExistingList(3, reqLi);
 
 // Attributes(Get/Set) - anything inside html tag. setAttribute getAttribute
 
-//.style for adding styles instead of overriding done by setAttribute. js cant have hyphenated property names so use camelcase property names as hypen will be considered as minus by js for a property name
+pTagElem[0].setAttribute("style", "color:red;");
+const myNameIs = listContainer.getAttribute("data-myNameIs");
+
+console.log("myNameIs>>>", myNameIs);
 
 // classes add/remove. classlist-> add, remove. work with active inactive buttons class. we can access elements stored using particular class selector even after removing that class from that element down the code but if we write another queryselector down the code we can;t select it but access it from previous variable where it is stored. so we store selected elemetns in a variable to access them down the code
+
+const activeListItem = document.querySelector(".active");
+console.log("activeListItem>>>", activeListItem);
+
+activeListItem.classList.remove("active");
+console.log("activeListItem>>>", activeListItem);
+
+const activeListItem2 = document.querySelector(".active");
+console.log("activeListItem2>>>", activeListItem2);
+
+activeListItem.classList.add("active");
+console.log("activeListItem>>>", activeListItem);
+
+const activeListItem3 = document.querySelector(".active");
+console.log("activeListItem3>>>", activeListItem3);
