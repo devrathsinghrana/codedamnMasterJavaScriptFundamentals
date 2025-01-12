030625602fd5187a0aa5f1fc274c93e4907d20a7
devrathrana <devrath.rana@bold.com>
2025-01-12 16:24:43 +0530

    regex

--- 12_formAndFormEvents/12_FormAndFormEvents.js
+++ 12_formAndFormEvents/12_FormAndFormEvents.js
@@ -38,3 +38,88 @@ const formSubmit = (ev) => {
   );
 };
 myFeedbackForm.addEventListener("submit", formSubmit);
+
+
+// Create the required functions here
+/**
+ * Returns an array of phone numbers in xxx xxx xxxx format
+ * @param {string} input - A string of text with phone numbers
+ * @returns {Array} An array of phoneNumbers
+ */
+function matchPhoneNumbers(input) {
+	const numberArray = input.split("").filter(item=>/^[0-9]$/.test(item))
+	const totalPhoneNumbers = numberArray.length/10;
+	const phoneNumbersArray=[]
+	let start=0;
+	let end=9;
+	for(let i=0;i<totalPhoneNumbers;i++){
+		const phoneNumerReq=[]
+		while(start<=end){
+			phoneNumerReq.push(numberArray[start])
+			start++;
+		}
+		start=end+1;
+		end=end+10;
+		phoneNumbersArray.push(phoneNumerReq.join(""))
+	}
+	return phoneNumbersArray;
+}
+
+/**
+ * Replaces a valid url with "URL_REPLACED"
+ * @param {string} input - A string of text with URLs
+ * @returns {string}
+ */
+function replaceURLs(input) {
+	return input.split(" ").filter(item=>/^http[s]{0,1}:[/]{2}[a-zA-Z.]{1,}.com$/.test(item))
+}
+
+/**
+ * Returns a count of number of words available in the give string
+ * @param {string} input - A string of text
+ * @returns {number}
+ */
+function countWords(input) {
+	return input.split(" ").filter(item=>/^[a-zA-Z]{1,}$/.test(item)).length
+}
+
+/**
+ * Returns the original string with a single space between words
+ * @param {string} input - A string of text with extra spaces
+ * @returns {string} cleaned string without any extra spaces
+ *
+ */
+function removeExtraSpaces(input) {
+	return input.split(" ").filter(item=>/^[a-zA-Z.]{1,}$/.test(item)).join(" ")
+}
+
+// Sample Test Cases
+console.log(
+	matchPhoneNumbers(
+		"My old phone number was 123 456 7890 but now you can reach me at 987 654 3210."
+	)
+);
+console.log(
+	matchPhoneNumbers("The store number is 555 321 9876, please call us.")
+);
+
+console.log(
+	replaceURLs(
+		"Find more info at https://example.com or contact us at http://help.example.com"
+	)
+);
+console.log(
+	replaceURLs(
+		"The website http://mysite.com has been moved to https://newsite.com"
+	)
+);
+
+console.log(countWords("This string has five words."));
+console.log(countWords("Counting words is easy!"));
+
+console.log(removeExtraSpaces("This   string    has   extra spaces."));
+console.log(
+	removeExtraSpaces(
+		"     Too many    spaces    at the beginning and end.     "
+	)
+);
