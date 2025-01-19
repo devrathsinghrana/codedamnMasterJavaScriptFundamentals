c231a82c1a7eb5793cc064b3616d88a160e65e86
devrathrana <devrath.rana@bold.com>
2025-01-19 13:06:17 +0530

    js

--- 14_arrayMethods.js
+++ 14_arrayMethods.js
@@ -88,4 +88,103 @@ const sumOfEvens = (arr) => {
 
 console.log("sumOfEvens>>>", sumOfEvens([-5, 0, 5, -4, 1, 6, -3, 2, 7]));
 
-//sort mutates original array
+//find return the first element that satisfies our condition
+// find num>50 in array of numbers
+
+//sort mutates original array - it is destructive array
+//if number of characters is same for each member of array then it will work fine else there is a problem as it sorts only on the basis of first character
+//we can sort an array by using custom comparision logic
+
+/**
+ * For ascending we have a-b-> a is first number and b is second number
+ *
+ *  result - order of numbers based on result is as follows
+ * +ve - a,b
+ * -ve - b,a
+ * 0 - no change
+ *
+ * Similarly for descending we have b-a
+ *
+ * So now we will sort movies in ascending order of their rating
+ */
+
+// made two objects and sort them separately as sort mutates original array
+const moviesKiLadiya = [
+  {
+    name: "inception",
+    rating: 8,
+  },
+  {
+    name: "madgaun express",
+    rating: 9,
+  },
+  {
+    name: "interstella",
+    rating: 8,
+  },
+  {
+    name: "kantara",
+    rating: 10,
+  },
+  {
+    name: "koi mil gya",
+    rating: 7,
+  },
+  {
+    name: "krissh 3",
+    rating: 3,
+  },
+];
+
+const moviesKiLadiyaTwo = [
+  {
+    name: "inception",
+    rating: 8,
+  },
+  {
+    name: "madgaun express",
+    rating: 9,
+  },
+  {
+    name: "interstella",
+    rating: 8,
+  },
+  {
+    name: "kantara",
+    rating: 10,
+  },
+  {
+    name: "koi mil gya",
+    rating: 7,
+  },
+  {
+    name: "krissh 3",
+    rating: 3,
+  },
+];
+
+const moviesKiLadiyaRatingAscending = moviesKiLadiya.sort((a, b) => {
+  return a.rating - b.rating;
+});
+
+const moviesKiLadiyaRatingDescending = moviesKiLadiyaTwo.sort((a, b) => {
+  return b.rating - a.rating;
+});
+
+function compareNumbersAsc(a, b) {
+  return a - b;
+}
+
+const ratingArray = [3, 6, 4, 8, 2, 1, 5, 10, 100000, 1000, 100000000, 15];
+const ratingArray2 = [3, 6, 4, 8, 2, 1, 5, 10, 100000, 1000, 100000000, 15];
+const ratingArrayAsc = ratingArray.sort(compareNumbersAsc);
+const ratingArrayDesc = ratingArray2.sort((a, b) => b - a);
+
+console.log("ratingArrayAsc>>>", ratingArrayAsc);
+console.log("ratingArrayDesc>>>", ratingArrayDesc);
+console.log("moviesKiLadiyaRatingAscending>>>", moviesKiLadiyaRatingAscending);
+
+console.log(
+  "moviesKiLadiyaRatingDescending>>>",
+  moviesKiLadiyaRatingDescending
+);
