5e32d2e8e01cfff120c3af3cd5b26762c772791a
devrathrana <devrath.rana@bold.com>
2025-01-20 23:48:22 +0530

    js

--- 16_DatesAndTimes.js
+++ 16_DatesAndTimes.js
@@ -17,3 +17,18 @@ console.log(now.toTimeString());
 console.log(now.toLocaleString());
 console.log(now.toLocaleDateString());
 console.log(now.toLocaleTimeString());
+
+const old = new Date("22 Feb, 2024 01:10:23");
+
+const oldTimeStampInMillisecond = old.getTime(); //Returns the stored time value in milliseconds since midnight, January 1, 1970 UTC.
+
+const newTimeStampInMillisecond = now.getTime();
+
+const millisecondDifference =
+  newTimeStampInMillisecond - oldTimeStampInMillisecond;
+
+const monthsDifference = Math.round(
+  millisecondDifference / 1000 / 60 / 60 / 24 / 30
+);
+
+console.log(`Blog is ${monthsDifference} months old!`);
