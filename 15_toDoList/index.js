69c958d6bda45336a5f17a72d84f739556bcaa82
devrathrana <devrath.rana@bold.com>
2025-01-20 22:00:27 +0530

    js

--- 15_toDoList/index.js
+++ 15_toDoList/index.js
@@ -1,78 +1,62 @@
 const addForm = document.querySelector(".add");
-const tasks = document.querySelector(".tasks");
-const clearAll = document.querySelector(".clear");
-const messageSpan = document.querySelector(".message span");
-const searchForm = document.querySelector(".search");
+const tasksList = document.querySelector(".tasks");
+const taskCountMessageSpan = document.querySelector(".message");
+const searchInputForm = document.querySelector(".search");
+const searchInput = document.querySelector(".search input");
+const searchInputClear = document.querySelector(".search .reset");
 
+//target add form and attach submit event. Submit value and reset form.
+const addFormSubmitHandler = (ev) => {
+  ev.preventDefault();
+  const taskAdded = ev.target.querySelector(`input[name="task"]`).value;
+  if (taskAdded) {
+    //target tasks list and add input value as li in it
+    const newLi = document.createElement("li");
+    newLi.innerHTML = `<span>${taskAdded}</span><i class="bi bi-trash-fill delete"></i>`;
+    tasksList.append(newLi);
+    addForm.reset();
+    updateMessage();
+  }
+};
 
-function updateMessage(){
-    const textLength = tasks.children.length;
-    messageSpan.textContent = `You have ${textLength} pending tasks.`;
-}
+addForm.addEventListener("submit", addFormSubmitHandler);
+
+//add click on ul instead of delete button and remove that li. Why? because if we target delete using class we will not be able to remove the whole li with delete button
+tasksList.addEventListener("click", (ev) => {
+  ev.target.parentNode.remove();
+  updateMessage();
+});
+
+//make clear all feature
+
+//update task count message
+const updateMessage = () => {
+  const tasksCount = tasksList.children.length;
+  const message = `You have ${tasksCount} pending task${
+    tasksCount > 1 ? "s" : ""
+  }.`;
+  taskCountMessageSpan.innerText = message;
+};
 updateMessage();
 
-
-addForm.addEventListener("submit", event => {
-    event.preventDefault();
-    const value = addForm.task.value.trim()
-
-    if(value.length){
-        tasks.innerHTML += `<li>
-                                <span>${value}</span>
-                                <i class="bi bi-trash-fill delete"></i>
-                            </li>`;
-        addForm.reset();
-        updateMessage();
+const showHideTasks = (inputValue = "") => {
+  const tasksArray = Array.from(tasksList.children);
+  tasksArray.forEach((task) => {
+    if (!task.innerText.toLowerCase().includes(inputValue)) {
+      task.classList.add("hide");
+    } else {
+      task.classList.remove("hide");
     }
+  });
+};
+//target searchbox and apply keyup to filter list
+searchInput.addEventListener("keyup", (ev) => {
+  const inputValue = ev.target.value;
+  showHideTasks(inputValue);
 });
 
-
-tasks.addEventListener("click", event => {
-    if(event.target.classList.contains("delete")){
-        event.target.parentElement.remove();
-        updateMessage();
-    }
+//clear filter form
+searchInputClear.addEventListener("click", () => {
+  searchInputForm.reset();
+  showHideTasks();
 });
-
-
-clearAll.addEventListener("click", event => {
-    const taskItems = tasks.querySelectorAll("li");
-    taskItems.forEach(item => {
-        item.remove();
-    });
-    updateMessage();
-});
-
-function filterTask(term){
-    Array.from(tasks.children)
-    .filter(task => {
-        return !task.textContent.toLowerCase().includes(term);
-    })
-    .forEach(task => {
-        task.classList.add("hide");
-    });
-
-    Array.from(tasks.children)
-    .filter(task => {
-        return task.textContent.toLowerCase().includes(term);
-    })
-    .forEach(task => {
-        task.classList.remove("hide");
-    });
-
-}
-
-
-searchForm.addEventListener("keyup", event => {
-    const term = searchForm.task.value.trim().toLowerCase();
-    filterTask(term);
-});
-
-
-searchForm.addEventListener("click", event => {
-    if(event.target.classList.contains("reset")){
-        searchForm.reset();
-        const term = searchForm.task.value.trim();
-        filterTask(term);
-    }
-})
